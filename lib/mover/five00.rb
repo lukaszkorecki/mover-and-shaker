66df4b8a454cf964bb2b414172c39c200670f6db
Łukasz Korecki <lukasz@korecki.me>
2016-05-22 11:24:16 +0000

    Shim 500px api access

--- lib/mover/five00.rb
+++ lib/mover/five00.rb
@@ -1,4 +1,14 @@
+require 'f00px'
+
 class Five00
-  def initialize config
+  def initialize(config)
+    @config = config
+    @f00 = ::F00px::Client.new
+
+#    @f00.xauth(config.f00px['login'], config.f00px['password'])
+
+
   end
+
+
 end
